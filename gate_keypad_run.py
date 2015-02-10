6a6647b591fdfc08ae4157d2df976585c87efde9
whitemyth <scott@msequine.com>
2015-02-09 22:44:02 -0500

    Create gate_keypad_run.py
    this is the keypad file to run - will need "gate_keypad_config.py" to work -  I cut out the choices for keypad style when adapting this so the functions might not be right  "kp = RPi_GPIO.keypad()" specifically

---
+++ gate_keypad_run.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+#gate_keypad_run.py
+
+#a script to run for the keypad - this will need "gate_keypad_config.py" in the same folder to work.
+
+from time import sleep
+import gate_keypad_config.py
+
+
+kp = RPi_GPIO.keypad()   # this did have a variable to select the type of keypad but since we do not need to select, I took it out, don't know if that was bad or not.
+
+def digit():
+	r= None
+	while r==None:   # keeps running until R has a value
+		r - kp.get()
+	return r
+	
+print "Please enter your passcode"
+
+d1 = digit()
+print d1
+sleep(.5)
+
+d2 = digit()
+print d2					#get and print digits - after testing is complete the printing should be changed to ****
+sleep(.5)
+
+d3 = digit()
+print d3
+sleep(.5)
+
+d4 = digit()
+print d4
+sleep(.5)
+
+print "you entered %s%s%s%s "%(d1,d2,d3,d4)  # use ?s instead of % when using sqlite
