c7682bbc858dbadf92f822add24c5c823257450a
Scott <scott@msequine.com>
2016-03-22 03:20:37 +0000

    updated i2c_keypad_test.py to test all digits with debounce

--- tests/i2c_keypad_test.py
+++ tests/i2c_keypad_test.py
@@ -1,46 +1,148 @@
 #!/usr/bin/env python3
 
 #from .usr.local.include.python2.7 
-import wiringpi2 as wiringpi
-from time import sleep
+import wiringpi as wiringpi
+from time import sleep   
+import time #only need time_stamp funciton - unknown name to import just that
 
-pin_base = 65       # lowest available starting number is 65
-i2c_addr = 0x24     # A0, A1, A2 pins all wired to GND
+pin_base = 65       # lowest available starting number is 65 (65=A0, 73=B0 etc)
+i2c_addr = 0x21     # A0, A1, A2 pins all wired to GND
 
 wiringpi.wiringPiSetup()                    # initialise wiringpi
 wiringpi.mcp23017Setup(pin_base,i2c_addr)   # set up the pins and i2c address
 
-#wiringpi.pinMode(65, 1)         # sets GPA0 to output
-#wiringpi.digitalWrite(65, 0)    # sets GPA0 to 0 (0V, off)
+#wiringpi.pinMode(x, y)        x=pin number  y=input or output (1=output 0=input) "wiringpi.pinmode(65,0)" pin 65 input 
+#wiringpi.digitalWrite(X, B)   X=pin number  B= 0ff/on (0v/3v3)  (0=off, 1=on)  "wiringpi.digitalWrite(65,0)"  pin 65 off
+#wiringpi.pullUpDncontro(X, D) X=pin number  D= internal pull up/dn/clear  2=up 1=dn 0=deactivate  
 
-wiringpi.pinMode(73, 0)         # sets GPB7 to input
-wiringpi.pinMode(74, 0)
-wiringpi.pinMode(75, 0)
-wiringpi.pinMode(76, 0)
-wiringpi.pinMode(65, 0)
-wiringpi.pinMode(66, 0)
+wiringpi.pinMode(72, 0)         # sets GPB7 to input
+wiringpi.pinMode(71, 0)
+wiringpi.pinMode(70, 0)
+wiringpi.pinMode(69, 0)
+wiringpi.pinMode(68, 0)
 wiringpi.pinMode(67, 0)
+wiringpi.pinMode(66, 0)
+wiringpi.pinMode(65, 1) #led for testing
 
-wiringpi.pullUpDnControl(73, 2) # set internal pull-up
-wiringpi.pullUpDnControl(74, 2)
-wiringpi.pullUpDnControl(75, 2)
-wiringpi.pullUpDnControl(76, 2)
-wiringpi.pullUpDnControl(65, 2)
-wiringpi.pullUpDnControl(66, 2)
-wiringpi.pullUpDnControl(67, 2) 
+time_stamp = time.time()
 
-# Note: MCP23017 has no internal pull-down, so I used pull-up and inverted
-# the button reading logic with a "not"
+#wiringpi.pullUpDnControl(73, 0) # set internal pull-up/dn/clear
+#wiringpi.pullUpDnControl(74, 0)
+#wiringpi.pullUpDnControl(75, 0)
+#wiringpi.pullUpDnControl(76, 0)
+#wiringpi.pullUpDnControl(65, 0)
+#wiringpi.pullUpDnControl(66, 0)
+#wiringpi.pullUpDnControl(67, 0) 
+
+#wiringpi.digitalWrite(80,0) # led on/off
+
 
 try:
     while True:
-        if not wiringpi.digitalRead(73): # inverted the logic as using pull-up
-            #wiringpi.digitalWrite(65, 1) # sets port GPA1 to 1 (3V3, on)
-            print ('test')
+        if  wiringpi.digitalRead(65) and wiringpi.digitalRead(68):
+            time_stamp       # put in to debounce  
+            time_now = time.time()  
+            if (time_now - time_stamp) >= 0.3:  
+                print ('1')
+            time_stamp = time_now    
+        elif wiringpi.digitalRead(66) and wiringpi.digitalRead(68):
+            time_stamp       # put in to debounce  
+            time_now = time.time()  
+            if (time_now - time_stamp) >= 0.3:  
+                print ('2')
+            time_stamp = time_now
+        elif wiringpi.digitalRead(67) and wiringpi.digitalRead(68):
+            time_stamp       # put in to debounce  
+            time_now = time.time()  
+            if (time_now - time_stamp) >= 0.3:  
+                print ('3')
+            time_stamp = time_now
+        elif wiringpi.digitalRead(65) and wiringpi.digitalRead(69):
+            time_stamp       # put in to debounce  
+            time_now = time.time()  
+            if (time_now - time_stamp) >= 0.3:  
+                print ('4')
+            time_stamp = time_now
+        elif wiringpi.digitalRead(66) and wiringpi.digitalRead(69):
+            time_stamp       # put in to debounce  
+            time_now = time.time()  
+            if (time_now - time_stamp) >= 0.3:  
+                print ('5')
+            time_stamp = time_now
+        elif wiringpi.digitalRead(67) and wiringpi.digitalRead(69):
+            time_stamp       # put in to debounce  
+            time_now = time.time()  
+            if (time_now - time_stamp) >= 0.3:  
+                print ('6')
+            time_stamp = time_now
+        elif wiringpi.digitalRead(65) and wiringpi.digitalRead(70):
+            time_stamp       # put in to debounce  
+            time_now = time.time()  
+            if (time_now - time_stamp) >= 0.3:  
+                print ('7')
+            time_stamp = time_now
+        elif wiringpi.digitalRead(66) and wiringpi.digitalRead(70):
+            time_stamp       # put in to debounce  
+            time_now = time.time()  
+            if (time_now - time_stamp) >= 0.3:  
+                print ('8')
+            time_stamp = time_now
+        elif wiringpi.digitalRead(67) and wiringpi.digitalRead(70):
+            time_stamp       # put in to debounce  
+            time_now = time.time()  
+            if (time_now - time_stamp) >= 0.3:  
+                print ('9')
+            time_stamp = time_now
+        elif wiringpi.digitalRead(65) and wiringpi.digitalRead(71):
+            time_stamp       # put in to debounce  
+            time_now = time.time()  
+            if (time_now - time_stamp) >= 0.3:  
+                print ('*')
+            time_stamp = time_now
+        elif wiringpi.digitalRead(66) and wiringpi.digitalRead(71):
+            time_stamp       # put in to debounce  
+            time_now = time.time()  
+            if (time_now - time_stamp) >= 0.3:  
+                print ('0')
+            time_stamp = time_now            
+        elif wiringpi.digitalRead(67) and wiringpi.digitalRead(71):
+            time_stamp       # put in to debounce  
+            time_now = time.time()  
+            if (time_now - time_stamp) >= 0.3:  
+                print ('#')
+            time_stamp = time_now   
         else:
-            wiringpi.digitalWrite(65, 0) # sets port GPA1 to 0 (0V, off)
+            wiringpi.digitalWrite(80, 0) 
         sleep(0.05)
 finally:
-    wiringpi.digitalWrite(65, 0) # sets port GPA1 to 0 (0V, off)
+    wiringpi.digitalWrite(80, 0) # sets port GPA1 to 0 (0V, off)
     wiringpi.pinMode(65, 0)      # sets GPIO GPA1 back to input Mode
-    # GPB7 is already an input, so no need to change anything
+
+
+
+'''
+try:
+    while True:
+        if  wiringpi.digitalRead(72): 
+            wiringpi.digitalWrite(80, 1) # sets port GPA1 to 1 (3V3, on)
+            print ('test')
+        else:
+            wiringpi.digitalWrite(80, 0) 
+        sleep(0.05)
+finally:
+    wiringpi.digitalWrite(80, 0) # sets port GPA1 to 0 (0V, off)
+    wiringpi.pinMode(65, 0)      # sets GPIO GPA1 back to input Mode
+'''
+'''
+try:
+    while True:
+        if  wiringpi.digitalRead(73): 
+            wiringpi.digitalWrite(80, 1) # sets port GPA1 to 1 (3V3, on)
+            print ('test')
+        else:
+            wiringpi.digitalWrite(80, 0) 
+        sleep(0.05)
+finally:
+    wiringpi.digitalWrite(80, 0) # sets port GPA1 to 0 (0V, off)
+    wiringpi.pinMode(65, 0)      # sets GPIO GPA1 back to input Mode
+'''    
